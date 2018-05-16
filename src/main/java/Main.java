a5adcd8171aa1478ae1f1069390aba19ac97277a
gf1721 <gf1721@bristol.ac.uk>
2018-05-16 20:40:55 +0100

    insertingdata

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -33,8 +33,36 @@ public class Main {
         }
     }
 
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C://sqlite/db/test.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public void insert(String name, double capacity){
+        String sql = "INSERT INTO warehouses(name,capacity) VALUES(?,?)";
+
+        try (Connection conn = this.connect();PreparedStatement pstmt = conn.prepareStatement(sql)){
+            pstmt.setString(1,name);
+            pstmt.setDouble(2,capacity);
+            pstmt.executeUpdate();
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
     public static void main(String[] args){
         //createNewDatabase("test.db");
-        createNewTable();
+        //createNewTable();
+        Main app = new Main();
+        app.insert("Raw Materials",3000);
+        app.insert("Semifinished Goods",4000);
+        app.insert("Finished Goods",5000);
     }
 }
