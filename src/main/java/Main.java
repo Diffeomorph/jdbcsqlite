e3fba65ac093743649525bc80dc20a8996c7f7ce
gf1721 <gf1721@bristol.ac.uk>
2018-05-16 20:32:28 +0100

    creatingtables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public static void createNewDatabase(String fileName){
@@ -18,7 +15,26 @@ public class Main {
         }
     }
 
+    public static void createNewTable(){
+        String url = "jdbc:sqlite:C://sqlite/db/tests.db";
+
+        String sql = "CREATE TABLE IF NOT EXISTS warehouses (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	name text NOT NULL,\n"
+                + "	capacity real\n"
+                + ");";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public static void main(String[] args){
-        createNewDatabase("test.db");
+        //createNewDatabase("test.db");
+        createNewTable();
     }
 }
