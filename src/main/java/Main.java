20063ad07a066b8c04375db512d3d35e5ed04202
gf1721 <gf1721@bristol.ac.uk>
2018-05-16 20:27:02 +0100

    creatingdatabase

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void createNewDatabase(String fileName){
+        String url = "jdbc:sqlite:C:/sqlite/db/"+fileName;
+
+        try (Connection conn = DriverManager.getConnection(url)){
+            if (conn != null){
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is "+meta.getDriverName());
+
+            }
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args){
+        createNewDatabase("test.db");
+    }
+}
